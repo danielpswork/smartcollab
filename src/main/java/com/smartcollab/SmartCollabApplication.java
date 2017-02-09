c3d11581e7f6243274a39569ec03ebe94a2e7bba
Daniel Paranhos <danielps@ciandt.com>
2017-02-09 09:13:26 -0200

    Commit inicial

---
+++ src/main/java/com/smartcollab/SmartCollabApplication.java
@@ -0,0 +1,22 @@
+package com.smartcollab;
+
+import java.security.Principal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SmartCollabApplication {
+
+	@RequestMapping("/user")
+	public Principal user(Principal principal) {
+		return principal;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCollabApplication.class, args);
+	}
+}

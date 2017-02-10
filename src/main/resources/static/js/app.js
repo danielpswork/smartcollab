c749dca776af45c5f37aed654409798cc3ad9c69
Daniel Paranhos <danielps@ciandt.com>
2017-02-10 17:39:39 -0200

    Colocando informações nos cards

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -38,19 +38,35 @@ $(document).ready(function() {
 
 function createCards() {
 	$('#cards').html('');
-	createCard('Card 1 ', 'Texto do Card 1', 5);
-	createCard('Card 2 ', 'Texto do Card 2', 5);
-	createCard('Card 3 ', 'Texto do Card 3', 5);
+	createCard(1, 'Card 1 ', 'danielps', 'Texto do Card 1 jkshf kjhsdf ksjdhdf kjsdkfjhskdj', '15/11/2016', 3);
+	createCard(2, 'Card 2 ', 'tfonseca', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '02/09/2016', 3);
+	createCard(3, 'Card 3 ', 'danielps', 'Texto do Card 3', '19/01/2017', 3);
+	createCard(4, 'Card 4 ', 'tfonseca', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '04/02/2017', 3);
+	createCard(5, 'Card 5 ', 'danielps', 'Texto do Card 5', '10/12/2016', 3);
 }
 
-function createCard(name, text, size) {
+function createCard(id, title, login, description, date, size) {
+	
+	var descriptionId = 'description_' + id;
 	
 	var cardHtml = '<div class="mdl-cell mdl-cell--' + size + '-col">';
 	cardHtml += '		<div class="demo-card-wide mdl-card mdl-shadow--2dp">';
 	cardHtml += '			<div class="mdl-card__title">';
-	cardHtml += '				<h2 class="mdl-card__title-text">' + name + '</h2>';
+	cardHtml += '				<h2 class="mdl-card__title-text">' + title + '</h2>';
 	cardHtml += '			</div>';
-	cardHtml += '		<div class="mdl-card__supporting-text">' + text + '</div>';
+	cardHtml += '		<div class="mdl-card__supporting-text">';
+	cardHtml += '			<div id="' + descriptionId + '">' + description.substring(0, 230);
+	if (description.length > 230) {
+		cardHtml += '...</div>';
+		cardHtml += '		<div class="mdl-tooltip mdl-tooltip--large" for="' + descriptionId + '">' + description + '</div>';
+	} else {
+		cardHtml += '</div>';
+	}
+	cardHtml += 			'<div style="position: absolute; bottom: 60px;" >';
+	cardHtml += 				'Por: ' + login + '<br/>';
+	cardHtml += 				'Data: ' + date;
+	cardHtml += 			'</div>';
+	cardHtml += '		</div>'
 	cardHtml += '		<div class="mdl-card__actions mdl-card--border">';
 	cardHtml += '			<a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">Moderar </a>';
 	cardHtml += '		</div>';

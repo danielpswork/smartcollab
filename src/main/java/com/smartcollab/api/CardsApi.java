94e1b3075e4bfdc4ab2cce4a7f0fa8129c26eaf5
estagiocit <estagiocit@cit>
2017-02-21 14:17:47 -0300

    Create method getById

--- src/main/java/com/smartcollab/api/CardsApi.java
+++ src/main/java/com/smartcollab/api/CardsApi.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -27,8 +28,14 @@ public class CardsApi {
         return service.getCards();
     }
 
+    @RequestMapping(path="/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public Card getCardById(@PathVariable(name="id") String id){
+        return service.getCardById(id);
+    }
+    
     @RequestMapping(method = RequestMethod.POST)
-    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseStatus(HttpStatus.OK)
     public String saveCard(@RequestBody Card card) {
         return service.saveCard(card);
     }
